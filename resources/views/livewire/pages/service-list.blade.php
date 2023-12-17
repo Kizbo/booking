9d7ea1e985e87452d80353ef5caf36fec6e23649
Madnutt <m.rupinski99@gmail.com>
2023-12-17 21:58:25 +0100

    calendar demo

--- resources/views/livewire/pages/service-list.blade.php
+++ resources/views/livewire/pages/service-list.blade.php
@@ -1,10 +1,8 @@
 @extends('front')
 @section('content')
-    <div>
-        If your happiness depends on money, you will never be happy with yourself.
-    </div>
     <div id="calendar"></div>
 @stop
 @section('javascript')
+    @vite(['resources/js/fullcalendar-locale/pl.js'])
     @vite(['resources/js/booking-calendar.js'])
 @stop
