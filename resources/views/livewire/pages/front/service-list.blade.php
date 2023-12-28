589c1c2cfd554c04508bbb5921172addff623848
Madnutt <m.rupinski99@gmail.com>
2023-12-28 16:00:39 +0100

    service list html

--- resources/views/livewire/pages/front/service-list.blade.php
+++ resources/views/livewire/pages/front/service-list.blade.php
@@ -1,10 +1,31 @@
-<div class="m-1">
-    <input type="text" wire:model="searchStr">
-    <button type="button" wire:click="search">ser</button>
-    @foreach ($services as $key => $service)
-        <livewire:pages.front.service-single :$service :key="$key" />
-        {{ $service->name }}
-    @endforeach
+<div class="mx-40 my-6">
+    <div class="flex justify-between mb-6">
+        <div>
+            <img src="#" alt="Logo">
+        </div>
+        <div class="flex rounded-full shadow-md shadow-gray-600">
+            <input type="text" class="rounded-l-full" wire:keydown.enter="search" wire:model="searchStr">
+            <button type="button" class="h-full border-r border-black rounded-r-full border-y bg-primary-gradient" wire:click="search">
+                <div class="p-2 mx-2 bg-black rounded-full">
+                    <x-fas-search class="w-4 fill-white" />
+                </div>
+            </button>
+        </div>
+    </div>
+    <div class="p-2 border border-black rounded-t-3xl bg-primary-gradient">
+        <div class="relative after:border after:border-black after:absolute after:rounded-t-3xl after:shadow-[inset_8px_8px_30px_-5px_#000] after:block after:top-0 after:w-full after:h-full">
+            <picture>
+                <source type="image/webp" srcset="{{ asset('images/sample_bakcground.webp') }}">
+                <img class="rounded-t-3xl" src="{{ asset('images/sample_bakcground.png') }}" alt="background" >
+            </picture>
+        </div>
+    </div>
+    <div class="relative z-10 grid grid-cols-4 -mt-6 border border-black rounded-3xl bg-primary [&>*]:border-r-2 [&>*]:border-black [&>*:nth-child(4)]:border-none [&>*:last-child]:border-none">
+        @foreach ($services as $key => $service)
+            <livewire:pages.front.service-single :$service :$key />
+        @endforeach
+    </div>
+    <x-modal :name="'book'">Content</x-modal>
 </div>
 
 @push('body-scripts')
--- resources/views/livewire/pages/front/service-single.blade.php
+++ resources/views/livewire/pages/front/service-single.blade.php
@@ -7,7 +7,22 @@ new class extends Component {
     public Service $service;
 }; ?>
 
-<div>
-    <h1>bebe</h1>
-    <h2>{{ $service->name }}</h2>
+<div class="flex flex-col py-3 pl-3 pr-6 my-3 ml-3 border-r border-black">
+    <div class="flex justify-between">
+        <h2 class="text-lg font-bold">{{ $service->name }}</h2>
+        <button type="button" @click="$dispatch('open-modal', 'book')" class="flex items-center justify-center bg-black rounded-full w-7 h-7">
+            <x-fas-calendar-plus class="w-3 fill-white" />
+        </button>
+    </div>
+    <p>{{ $service->description }}</p>
+    <div class="flex items-end justify-end flex-grow mt-1 gap-x-4">
+        <div class="flex flex-col items-center">
+            <x-fas-stopwatch class="w-6" />
+            <p class="font-bold">{{ $service->duration }} min</p>
+        </div>
+        <div class="flex flex-col items-center">
+            <x-fas-money-bill-1-wave class="w-6" />
+            <p class="font-bold">{{ $service->price }} zł</p>
+        </div>
+    </div>
 </div>
